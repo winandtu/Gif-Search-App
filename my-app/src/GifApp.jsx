71ba51be918489a575ef94737e5b51022ca4e36a
Danilo Arévalo <jyfy121@gmail.com>
2024-01-05 18:17:33 -0500

    Css para el DOM

--- my-app/src/GifApp.jsx
+++ my-app/src/GifApp.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 export const GifApp = () => {
+
+  const [categories, setCategories] = useState(['One punch man', 'Dragon Ball'])
+
+  //Función para agregar categoria
+
+  const addCategorie = ()=>{
+    
+    const newCategorie='Naruto';
+    setCategories([newCategorie, ...categories ]) //hace una copia de las categorias anteriores y agrega una nueva
+    //Segunda forma
+    //setCategories(cat =>[...cat, newCategorie])
+    console.log('Se ha agregado algo')
+    
+    
+  }
+
+  //console.log(categories)
   return (
     <>
-    <div>GifApp</div>
+    <h1>GifApp</h1>
+
+    {/**Agregar categorias por medio de un botón */}
+
+    <button onClick={addCategorie}>Agregar Categoria</button>
+
+    {/**Lista de las categorias */}
+    <ol>
+      {categories.map(category=>{
+        return <li key={category}>{category}</li>
+      })}
+    </ol>
     </>
   )
 }
