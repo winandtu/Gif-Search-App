a3efb0ec5f565b74dc53b396a22591aa637c1a3a
Danilo Arévalo <jyfy121@gmail.com>
2024-01-09 00:24:10 -0500

    Funcion para obtener imgs

--- my-app/src/components/Gif.jsx
+++ my-app/src/components/Gif.jsx
@@ -1,17 +1,46 @@
 import React from 'react'
 import { getGif } from '../helpers/Api';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export const Gif = (props) => {
 
-    const {category}=props;
+    const { category } = props;
 
+    //Estado para obtener las imgs
+    const [images, setImages] = useState([])
+
+    //Hago una función que me retorne las imagenes
+
+    const getImages = async () => {
+
+        const newImage = await getGif(category); //Obtengo la respuesta
+        setImages(newImage); //Actualizo las imagenes
+
+    }
     //Aqui va el componente de la API
-    getGif(category);
+    // Realizar la solicitud HTTP cuando el componente se monta
 
-  return (
-    <>
-    <h3>{category}</h3>
+    useEffect(() => {
+        getImages(); //LLamo la función
+    }, [])
 
-    </>
-  )
+
+
+
+    return (
+        <>
+            <h3>{category}</h3>
+            <ol>
+                {/**Titulos de las imagenes */}
+                {images.map(({id, title}) => {
+                    return <li key={id}>
+
+                        {title}
+                    </li>
+                })}
+            </ol>
+
+        </>
+    )
 }
